43854ef68db6b6a4a703d8724883d047d841f21c
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-20 21:17:24 +0300

    for loops

---
+++ for_loops.rb
@@ -0,0 +1,13 @@
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+
+for friend in friends
+    puts friend
+end
+
+for num in 0..5
+    puts num
+end
+
+3.times do |index|
+    puts index
+end
