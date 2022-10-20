41523e209b557a132a78c886bb083a9475980739
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-20 13:38:53 +0300

    different variable declaration and invocation methods

--- variables.rb
+++ variables.rb
@@ -8,6 +8,8 @@
  name = "Kevin"
  city = "Nairobi"
  favorite_language = "Ruby"
+ age = "26"
 
- puts "Hello #{name}!"
- puts "I hear you live in #{city} and your favorite programming language is #{favorite_language}."
+ puts "Hello, my name is #{name}!"
+ puts ("I am "+ age +" years old")
+ puts "I live in #{city} and my favorite programming language is #{favorite_language}."
