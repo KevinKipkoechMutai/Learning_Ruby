593eb16c0b90f5db8017125be106f68a42f2743b
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-20 17:00:07 +0300

    arrays

---
+++ arrays.rb
@@ -0,0 +1,14 @@
+friends = Array["Kevin", "Karen", "Oscar"]
+puts friends
+
+teams = ["Chelsea", "Arsenal", "Tottenham", "Liverpool"]
+puts teams[0]
+puts teams[-1]
+
+concatenated = friends.concat(teams)
+puts concatenated
+friends[0] = "Daisy"
+friends[3] = "David"
+friends[5] = "David"
+
+puts friends
