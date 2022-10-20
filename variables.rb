8fa1551214991a47493ab7a27bc40c406f8191aa
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-20 13:30:01 +0300

    declaring variables

---
+++ variables.rb
@@ -0,0 +1,13 @@
+#printing a shape in ruby
+ puts "   /|"
+ puts "  / |"
+ puts " /  |"
+ puts "/___|"
+
+ #variable declaration
+ name = "Kevin"
+ city = "Nairobi"
+ favorite_language = "Ruby"
+
+ puts "Hello #{name}!"
+ puts "I hear you live in #{city} and your favorite programming language is #{favorite_language}."
