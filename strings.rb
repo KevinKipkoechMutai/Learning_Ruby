3a62998a7adcc0cb8bde38271671656d9160f0cf
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-20 13:54:35 +0300

    working with strings

---
+++ strings.rb
@@ -0,0 +1,16 @@
+#printing
+puts "Flatiron School"
+puts "Moringa School"
+
+#using variables
+phrase = "software engineering"
+puts phrase
+
+puts phrase.upcase
+puts phrase.downcase
+puts phrase.length
+puts phrase.include?"engineering"
+puts phrase[1]
+puts phrase[0, 4]
+puts phrase[0, 4].upcase
+puts phrase.index("engine")
