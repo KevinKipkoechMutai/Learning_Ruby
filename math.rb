341c47d2490fa59968238e0a16522a5358aa8a55
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-20 14:06:00 +0300

    math operations in ruby

---
+++ math.rb
@@ -0,0 +1,17 @@
+puts 2 + 2
+puts 2 ** 3
+puts 10 % 3
+
+num = -10
+puts ("My favorite number is " + num.to_s)
+
+puts num.abs
+
+num1 = 20.1
+puts num1.round
+puts num1.ceil
+puts num1.floor
+
+
+puts Math.sqrt(16)
+puts 1.0 + 8
