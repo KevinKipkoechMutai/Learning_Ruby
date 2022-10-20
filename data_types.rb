d8ccf6aafd1b659443c5e45b7dd955dd68382f93
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-20 13:44:01 +0300

    data types in ruby

---
+++ data_types.rb
@@ -0,0 +1,14 @@
+#strings => plain text
+name = "Kevin"
+occupation = "software engineer"
+
+#numbers => numerical data
+age = 75 #integer
+height = 5.98 #float
+
+#boolean
+ismale = true
+isfemale = false
+
+#undefined => no value
+flaws = nil
