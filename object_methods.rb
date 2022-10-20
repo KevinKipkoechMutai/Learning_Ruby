97d063902d809d632bd252a9e26d191638006897
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-20 22:20:47 +0300

    object methods

---
+++ object_methods.rb
@@ -0,0 +1,23 @@
+class Student
+    attr_accessor :name, :major, :gpa
+    def initialize(name, major, gpa)
+        @name = name
+        @major = major
+        @gpa = gpa
+    end
+
+    def has_honors
+        if @gpa >= 3.5
+            true
+        else
+            false
+        end
+        
+    end
+end
+
+student1 = Student.new("James", "Business", 3.1)
+student2 = Student.new("Pam", "Art", 3.6)
+
+puts student1.has_honors
+puts student2.has_honors
