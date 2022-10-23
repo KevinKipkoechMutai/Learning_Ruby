ae605995b81b9fefe618819eefecbb228ed4eb63
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-23 19:53:53 +0300

    OOP in ruby

---
+++ ooruby.rb
@@ -0,0 +1,34 @@
+#creating a class
+class Point
+    #initializing
+    def initialize(x, y)
+        @x, @y = x, y
+    end
+
+    #method
+    def to_s
+        "(#{@x}, #{@y})"
+    end
+
+    def x
+        @x
+    end
+
+    def x=(value)
+        @x = value
+    end
+
+    def y
+        @y
+    end
+
+    def y=(value)
+        @y = value
+    end
+end
+
+#instantiating a point
+p = Point.new(1,2)
+
+puts p
+
